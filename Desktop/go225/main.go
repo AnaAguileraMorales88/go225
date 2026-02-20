bac242821b4a5d3f2b73d86b6d01da510c144250
Ana Aguilera <anitaaguilera@hotmail.com>
2026-02-20 18:28:26 +0100

    Primer commit

---
+++ Desktop/go225/main.go
@@ -0,0 +1,23 @@
+package main
+
+
+import "fmt"
+
+//Creación variable con tipos definidos
+var edad int = 37
+
+func main(){
+	//Creación variable
+	nombre := "Ana"
+
+	a,b,c := 4,"hola",true
+
+	fmt.Println("Hola clase, mi nombre es " + nombre)
+
+}
+
+func saludar(){
+	fmt.Println(edad)
+
+
+}
