b5801868d9e3cd3e5fc1b39662eda39142deca06
Ana Aguilera <anitaaguilera@hotmail.com>
2026-02-23 16:11:21 +0100

    Ejercico go

---
+++ Desktop/go225/exercici7prev.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	edad := 23
+
+	if edad >= 18 {
+		fmt.Println("Puedes pasar")
+	} else {
+		fmt.Println("No puedes pasar")
+	}
+}
+
+
+//Amb aquest exercici tens que realitzar una estructura condicional que representi un porter de discoteca que nomès deixa passar si ets major d'edat.
